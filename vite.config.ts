d4801e006f209fa0b5ed4703049f6d875e60c3a3
Abdulrahman <abdalissa2005hotmail.com@gmail.com>
2026-03-08 22:01:47 +0200

    update site
    git push origin main#

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,16 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
+import { cloudflare } from "@cloudflare/vite-plugin";
+
 export default defineConfig({
   server: {
     host: "::",
     port: 8080,
     hmr: { overlay: false },
   },
-  plugins: [
-    react(),
-    componentTagger(), // always include it; it won’t break production
-  ],
+  plugins: [react(), // always include it; it won’t break production
+  componentTagger(), cloudflare()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
